11695c95ad6090e18147bbc5628f2a2f000d6459
Asif9060 <moodyfoisal@gmail.com>
2026-09-11 15:33:50 +0600

    Implemented Navbar Component

---
+++ src/components/Nav.tsx
@@ -0,0 +1,30 @@
+import logo from "../assets/logo-text.png"
+const Nav = () => {
+    return (
+        <div>
+            <nav className="max-w-7xl mx-auto flex justify-between items-center mt-6">
+                <div>
+                    <img src={logo} alt="" />
+                </div>
+
+                <div>
+                    <ul className="flex gap-7 font-semibold">
+                        <li className="text-[#DB2777]"><a href="">Home</a></li>
+                        <li><a href="">Technologies</a></li>
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div className="flex">
+                    <button className="cursor-pointer">Sign In</button>
+                    <button className=" bg-[#D91B7E] text-white rounded-4xl ml-3 py-2.5 px-5 cursor-pointer">Sign Up</button>
+                </div>
+            </nav>
+
+        </div>
+    );
+};
+
+export default Nav;
